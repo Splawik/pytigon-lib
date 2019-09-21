8ff1ef7510b427ccf58de8bf39b92d8a811d337b
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2019-09-21 17:00:20 +0200

    fixed: setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description="Pytigon library",
     author="Sławomir Chołaj",
     author_email="slawomir.cholaj@gmail.com",
+    license="LGPLv3",
     packages=find_packages(),
     install_requires=[
         "fpdf",
@@ -19,5 +20,18 @@ setup(
         "cffi",
         "Pillow",
         "lxml",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
