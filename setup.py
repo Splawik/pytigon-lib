a91341d9e600993de57d9ab748a95d52c905b0fa
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2019-12-10 21:53:49 +0100

    small changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytigon-lib",
-    version="0.94",
+    version="0.95",
     description="Pytigon library",
     author="Sławomir Chołaj",
     author_email="slawomir.cholaj@gmail.com",
