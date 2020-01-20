7470a0e4253007ab3eba52fe87e8c58c51b95890
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2020-01-20 16:03:16 +0100

    Small changes

--- pytigon_lib/schbrowser/cef_run.py
+++ pytigon_lib/schbrowser/cef_run.py
@@ -1,11 +1,10 @@
-# Hello world example. Doesn't depend on any third party GUI framework.
-# Tested with CEF Python v57.0+.
-
 from cefpython3 import cefpython as cef
 import platform
 import sys
 import time
 import http.client
+import asyncio
+import json
 
 def exists(site, path="/"):
     try:
@@ -32,13 +31,36 @@ def check_versions():
     assert cef.__version__ >= "57.0", "CEF Python v57.0+ required to run this"
 
 
-def run(url, title="Pytigon", x=200, y=200, width=1024, height=768):
-    check_versions()
-    sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
-    cef.Initialize()
+async def run_async(url, title="Pytigon", parent_win = None, x=200, y=200, width=1024, height=768):
+    close = False
+    
+    class LifespanHandler(object):
+        def OnBeforeClose(self, browser):
+            nonlocal close 
+            close = True
+
+    class JSBridge:
+        def __init__(self, eval_events):
+            self.results = {}
+            self.eval_events = eval_events
+
+        def return_result(self, result, uid):
+            self.results[uid] = json.loads(result) if result else None
+            self.eval_events[uid].set()
+
+        def call(self, func_name, param, value_id):
+            js_bridge_call(self.window, func_name, param, value_id)
+
+    async def loop():
+        nonlocal close 
+        while not close:
+            cef.MessageLoopWork()
 
     info = cef.WindowInfo()
-    info.SetAsChild(0, [x, y, x + width, y + height])
+    if parent_win:
+        info.SetAsChild(self.Handle, [x, y, x + width, y + height])
+    else:
+        info.SetAsChild(0, [x, y, x + width, y + height])
 
     while True:
         test = exists(url)
@@ -49,7 +71,20 @@ def run(url, title="Pytigon", x=200, y=200, width=1024, height=768):
             time.sleep(1)
             print("Sleep")
 
-    cef.CreateBrowserSync(window_info=info, url=url, window_title=title)
-    cef.MessageLoop()
-    cef.Shutdown()
+    browser = cef.CreateBrowserSync(window_info=info, url=url, window_title=title)
+    browser.SetClientHandler(LifespanHandler())
 
+    bindings = cef.JavascriptBindings()
+    bindings.SetObject('external', browser.js_bridge)
+
+    bindings.SetFunction('alert', alert_func)
+
+    await loop()
+
+
+def run(url, title="Pytigon", parent_win = None, x=200, y=200, width=1024, height=768):
+    check_versions()
+    sys.excepthook = cef.ExceptHook
+    cef.Initialize()
+    asyncio.run(run_async(url, title, parent_win, x, y, width, height))
+    cef.Shutdown()
