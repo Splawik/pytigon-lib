ed61b30ab3f5be3206fef2185d0373404e33e933
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 16:10:31 +0000

    Adding tarfile member sanitization to extractall()

--- pytigon_lib/schtools/cc.py
+++ pytigon_lib/schtools/cc.py
@@ -79,7 +79,26 @@ def install_tcc(path):
             url = "http://download.savannah.gnu.org/releases/tinycc/tcc-0.9.27.tar.bz2"
             r = httpx.get(url, allow_redirects=True)
             with tarfile.open(fileobj=io.BytesIO(r.content), mode="r:bz2") as tar:
-                tar.extractall(prg_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, prg_path)
             os.rename(os.path.join(prg_path, "tcc-0.9.27"), path)
             temp = os.getcwd()
             os.chdir(path)
